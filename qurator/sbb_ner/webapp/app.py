29eb154fb8e4f3edb12e7b0557887f38905f2f1b
Kai Labusch <labusch@gmail.com>
2019-12-13 11:17:45 +0100

    more correct interpretation of BERT tokens

--- qurator/sbb_ner/webapp/app.py
+++ qurator/sbb_ner/webapp/app.py
@@ -238,6 +238,9 @@ def ner(model_id=None):
 
                 continue
 
+            if not token.startswith('##') and word_pred == 'X':
+                word_pred = 'O'
+
             token = token[2:] if token.startswith('##') else token
 
             word += token
